f96c96fe0ba6f38f7e6de337313375b26b41252b
Iurii Grytsaienko <iuriigrytsaenko@gmail.com>
2016-10-13 23:38:31 +0300

    add drawing function

--- script.js
+++ script.js
@@ -4,12 +4,30 @@ function initRectangle(rec) {
     }
 };
 
+var cx = document.querySelector("canvas").getContext("2d");
+
+var scaleCoefficient = 30;
+
+function drawRectangle(obj, sc) {
+    cx.strokeRect(obj.left * sc , obj.bottom * sc , (obj.right - obj.left) * sc, (obj.top - obj.bottom) * sc );
+};
+
+function drawStrokeEllipse(x, y, a, b, sc){
+    cx.beginPath();
+    cx.ellipse(x * sc, y * sc, a * sc, b * sc, 0, 0, 2 * Math.PI);
+    cx.stroke();
+};
+
+function drawFillEllipse(x, y, a, b, sc){
+    cx.beginPath();
+    cx.ellipse(x * sc, y * sc, a, b, 0, 0, 2 * Math.PI);
+    cx.fill();
+};
+
 let rectangle1 = {x1 : null, y1 : null, x2 : null, y2 : null};
 let rectangle2 = {x1 : null, y1 : null, x2 : null, y2 : null};
 
 let array = [];
-
-var cx = document.querySelector("canvas").getContext("2d");
 
 initRectangle(rectangle1);
 initRectangle(rectangle2);
@@ -28,8 +46,6 @@ let r2 = {
     bottom: Math.min(rectangle2.y1, rectangle2.y2)
 };
 
-
-
 if ( !((r1.left >= r2.right)||(r1.right <= r2.left)||(r1.top <= r2.bottom)||(r1.bottom >= r2.top)) ) {
     var r3 = {
         left: Math.max(r1.left, r2.left),
@@ -43,30 +59,21 @@ if ( !((r1.left >= r2.right)||(r1.right <= r2.left)||(r1.top <= r2.bottom)||(r1.
     let x0 = r3.left + a;
     let y0 = r3.bottom + b;
 
-    cx.beginPath();
-    cx.ellipse(x0*30, y0*30, a*30, b*30, 0, 0, 2 * Math.PI);
-    cx.stroke();
-
-
+    drawStrokeEllipse(x0, y0, a, b, scaleCoefficient);
 
     while (array.length < 10) {
         var x = Math.random()*(r3.right - r3.left) + r3.left;
         var y = Math.random()*(r3.top - r3.bottom) + r3.bottom;
         if ( (((x-x0) ** 2)/a/a + ((y-y0) ** 2)/b/b) <1 ) {
             array.push({x: x, y: y});
-            cx.beginPath();
-            cx.ellipse(x*30, y*30, 5, 5, 0, 0, 2 * Math.PI);
-            cx.fill();
+            drawFillEllipse(x, y, 5, 5, scaleCoefficient);
         }
     }
-
-
 };
 
 for (var key in array) {
     console.log(array[key]);
 };
 
-
-cx.strokeRect(r1.left * 30 , r1.bottom * 30 , (r1.right - r1.left) * 30, (r1.top - r1.bottom) * 30 );
-cx.strokeRect(r2.left * 30 , r2.bottom * 30 , (r2.right - r2.left) * 30, (r2.top - r2.bottom) * 30);
+drawRectangle(r1, scaleCoefficient);
+drawRectangle(r2, scaleCoefficient);
