f3e2efb3b24b6ad48290473d1d9570f3dde3a2d9
Iurii Grytsaienko <iuriigrytsaenko@gmail.com>
2016-10-23 19:15:35 +0300

    change object initialization, move draw function in bottom, delete some semicolons

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
-function initRectangle(rec) {
-    for (var key in rec) {
-        rec[key] = Math.floor(Math.random()*10);
-    }
-};
+// function initRectangle(rec) {
+//     for (var key in rec) {
+//         rec[key] = Math.floor(Math.random() * 10);
+//     }
+// }
+
+function initCoordinate() {
+    return Math.floor(Math.random() * 10);
+}
 
 var cx = document.querySelector("canvas").getContext("2d");
 
 var scaleCoefficient = 30;
 
-function drawRectangle(obj, sc) {
-    cx.strokeRect(obj.left * sc , obj.bottom * sc , (obj.right - obj.left) * sc, (obj.top - obj.bottom) * sc );
-};
-
-function drawStrokeEllipse(x, y, a, b, sc){
-    cx.beginPath();
-    cx.ellipse(x * sc, y * sc, a * sc, b * sc, 0, 0, 2 * Math.PI);
-    cx.stroke();
-};
-
-function drawFillEllipse(x, y, a, b, sc){
-    cx.beginPath();
-    cx.ellipse(x * sc, y * sc, a, b, 0, 0, 2 * Math.PI);
-    cx.fill();
-};
-
-let rectangle1 = {x1 : null, y1 : null, x2 : null, y2 : null};
-let rectangle2 = {x1 : null, y1 : null, x2 : null, y2 : null};
+let rectangle1 = {x1: initCoordinate(), y1: initCoordinate(), x2: initCoordinate(), y2: initCoordinate()};
+let rectangle2 = {x1: initCoordinate(), y1: initCoordinate(), x2: initCoordinate(), y2: initCoordinate()};
 
 let array = [];
-
-initRectangle(rectangle1);
-initRectangle(rectangle2);
 
 let r1 = {
     left: Math.min(rectangle1.x1, rectangle1.x2),
@@ -46,7 +31,7 @@ let r2 = {
     bottom: Math.min(rectangle2.y1, rectangle2.y2)
 };
 
-if ( !((r1.left >= r2.right)||(r1.right <= r2.left)||(r1.top <= r2.bottom)||(r1.bottom >= r2.top)) ) {
+if (!((r1.left >= r2.right) || (r1.right <= r2.left) || (r1.top <= r2.bottom) || (r1.bottom >= r2.top))) {
     var r3 = {
         left: Math.max(r1.left, r2.left),
         top: Math.min(r1.top, r2.top),
@@ -54,26 +39,44 @@ if ( !((r1.left >= r2.right)||(r1.right <= r2.left)||(r1.top <= r2.bottom)||(r1.
         bottom: Math.max(r1.bottom, r2.bottom)
     };
 
-    let a = (r3.right - r3.left)/2;
-    let b = (r3.top - r3.bottom)/2;
+    let a = (r3.right - r3.left) / 2;
+    let b = (r3.top - r3.bottom) / 2;
     let x0 = r3.left + a;
     let y0 = r3.bottom + b;
 
     drawStrokeEllipse(x0, y0, a, b, scaleCoefficient);
 
     while (array.length < 10) {
-        var x = Math.random()*(r3.right - r3.left) + r3.left;
-        var y = Math.random()*(r3.top - r3.bottom) + r3.bottom;
-        if ( (((x-x0) ** 2)/a/a + ((y-y0) ** 2)/b/b) <1 ) {
+        var x = Math.random() * (r3.right - r3.left) + r3.left;
+        var y = Math.random() * (r3.top - r3.bottom) + r3.bottom;
+        if ((((x - x0) ** 2) / a / a + ((y - y0) ** 2) / b / b) < 1) {
             array.push({x: x, y: y});
             drawFillEllipse(x, y, 5, 5, scaleCoefficient);
         }
     }
-};
+}
+;
 
 for (var key in array) {
     console.log(array[key]);
-};
+}
+;
 
 drawRectangle(r1, scaleCoefficient);
 drawRectangle(r2, scaleCoefficient);
+
+function drawRectangle(obj, sc) {
+    cx.strokeRect(obj.left * sc, obj.bottom * sc, (obj.right - obj.left) * sc, (obj.top - obj.bottom) * sc);
+}
+
+function drawStrokeEllipse(x, y, a, b, sc) {
+    cx.beginPath();
+    cx.ellipse(x * sc, y * sc, a * sc, b * sc, 0, 0, 2 * Math.PI);
+    cx.stroke()
+}
+
+function drawFillEllipse(x, y, a, b, sc) {
+    cx.beginPath();
+    cx.ellipse(x * sc, y * sc, a, b, 0, 0, 2 * Math.PI);
+    cx.fill();
+}
